81548cea6537e0040a2c2a3d899c2bf034097734
Prisha Bhanot <bhanop1@mcmaster.ca>
2025-02-20 23:42:05 -0500

    color dictionary

--- Code.py
+++ Code.py
@@ -8,6 +8,23 @@ print (df.head())
 
 labels = df['Species']
 
-x = df.drop(["Id", "Species"], axis=1)
+X  = df.drop(["Id", "Species"], axis=1)
 
 #Center all xi around orgin 
+X_std = StandardScaler().fit_transform(X)
+
+pca = PCA(n_components=2)
+
+X_transform = pca.fit_transform(X_std)
+
+print (pca.explained_variance_ratio_)
+print(X_transform)
+
+pca1 = zip(*X_transform)[0]
+pca2 = zip(*X_transform)[1]
+
+color_dict={}
+color_dict["Iris-sentosa"]="green"
+color_dict["Iris-versicolor"]="green"
+color_dict["Iris-verginica"]="green"
+
